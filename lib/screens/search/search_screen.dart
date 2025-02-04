bd33211c950ac257ac7f74b74b9fe4a2419a9681
ourobf <ourobf2012@gmail.com>
2025-02-04 13:05:16 +0000

    commit-2

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -6,6 +6,7 @@ import 'package:tichet_app/base/widgets/app_double_text.dart';
 import 'package:tichet_app/screens/search/widgets/app_text_icon.dart';
 import 'package:tichet_app/screens/search/widgets/app_ticket_tabs.dart';
 import 'package:tichet_app/screens/search/widgets/find_tickets.dart';
+import 'package:tichet_app/screens/search/widgets/ticket_promotion.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -58,50 +59,7 @@ class SearchScreen extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          Row(
-            children: [
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                width: size.width * .42,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade200,
-                          blurRadius: 1,
-                          spreadRadius: 2),
-                    ]),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          image: const DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage(AppMedia.planeSit))),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      "20% discount on the early booking of this flight. Don't miss.",
-                      style: AppStyles.headLineStyle2,
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: size.width * .44,
-                  )
-                ],
-              ),
-            ],
-          ),
+          const TicketPromotion(),
         ],
       ),
     );
